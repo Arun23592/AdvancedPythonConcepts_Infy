bdcd26f53472a3468098fee7b973257a0c0437bc
Arun23592 <arunsubramani1992@gmail.com>
2024-01-03 22:04:20 +0530

    commit

--- SeleniumPython/Webdriver.py
+++ SeleniumPython/Webdriver.py
@@ -1,5 +1,14 @@
-from selenium import webdriver
+import time
 
-driver = webdriver.Chrome(executable_path="F:\\ChromeDriver\\chromedriver-win64\\chromedriver.exe")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
 driver.get("https://www.ey.com/en_us")
 print(driver.title)
+driver.find_element(By.xpath, '//*[text()="I accept all cookies"]').click()
+driver.find_element(By.xpath, '//*[text()="Search"]').click()
+driver.find_element(By.xpath, "//input[@type='search']").send_keys("AI")
+time.sleep(5)
+
+
